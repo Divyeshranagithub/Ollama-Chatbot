e362f42e5bb8f1eab85a60ed0b4695b39844e222
Divyesh Ranaa <ranadev498@gmail.com>
2024-06-21 07:54:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# 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
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
@@ -11,10 +10,10 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-st.title('Langchain ollama chatbot')
+st.title('Ollama Chatbot')
 input_t = st.text_input('Enter your question here:')
 
-llm=Ollama(model='gemma:2b') 
+llm=Ollama(model='gemma:2b', base_url="http://ollama-container:11434", verbose=True)
 output = StrOutputParser()
 
 chain = prompt|llm|output
